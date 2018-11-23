c604ddba02a93611d3b910e3dafbc49dfcf9883f
LastContinue <last@continue.com>
2018-11-23 13:33:28 -0600

    Updating all scripts to work with mdloader 1.0.3

--- scripts/powershell/ctrl-bootstrap-windows.ps1
+++ scripts/powershell/ctrl-bootstrap-windows.ps1
@@ -3,9 +3,9 @@ $newDir = $desktop+'\ctrl'
 New-Item -Path $newDir -ItemType Directory -Force
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/Massdrop/mdloader/releases/download/0.0.1/mdloader_windows.exe -OutFile $newdir"\mdloader_windows.exe"
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/Massdrop/mdloader/releases/download/0.0.1/applet-flash-samd51j18a.bin -OutFile $newdir"\applet-flash-samd51j18a.bin"
-Invoke-WebRequest -UseBasicParsing -Uri https://gist.githubusercontent.com/LastContinue/5f8fb23b41b1611e49d4aced8a44101e/raw/986083d1020f8b1d8ac50e6e822bb0872f96734e/pwr-ldr.ps1 -OutFile $newdir"\pwr-ldr.ps1"
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/Massdrop/mdloader/releases/download/1.0.3/mdloader_windows.exe -OutFile $newdir"\mdloader_windows.exe"
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/Massdrop/mdloader/releases/download/1.0.3/applet-flash-samd51j18a.bin -OutFile $newdir"\applet-flash-samd51j18a.bin"
+Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/LastContinue/ctrl-info/master/scripts/powershell/pwr-ldr.ps1 -OutFile $newdir"\pwr-ldr.ps1"
 
 $w = New-Object -ComObject WScript.Shell
 $link = $w.CreateShortcut("$desktop\pwr-ldr.lnk")
--- scripts/powershell/pwr-ldr.ps1
+++ scripts/powershell/pwr-ldr.ps1
@@ -1,16 +1,11 @@
 #Windows version of the ctrl-ldr script.
-#this will all you to flash the board without having to have two keyboards
-#and only running one command 
-#Make sure you can run PS scripts by running 
+#Updated to target mdloader 1.0.3
+#You may get permission prompts if this is the first time you've ran this
+#If these don't stop, try running 
 #Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force 
-#beforehand 
-#or use companion .bat file
+#in Powershell before running this.
 param(
     [Parameter(ValueFromRemainingArguments=$true)]$fileName
 )
 echo "Starting Flashing Procedure with File $fileName"
-echo "Starting in 10 seconds (push the button)"
-Start-Sleep 12
-$device=.\mdloader_windows.exe --list | findstr -i COM | %{ $_.Split(' ')[2]; }
-echo "Found device on $DEVICE"
-.\mdloader_windows.exe --port $device --download $fileName --restart
+.\mdloader_windows.exe --first --download $fileName --restart
